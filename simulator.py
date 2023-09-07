0e4db73cf6e413dc469806df839101599c6f91de
Mac-Coleman <williamcormaccoleman@gmail.com>
2023-09-07 16:38:42 -0500

    Removed argparse, added support for -h and -v flags

--- simulator.py
+++ simulator.py
@@ -5,13 +5,25 @@ This file must be run in order to start the simulator.
 
 import sys
 
-from cli_parser import parse_arguments
+from cli_parser import parse_arguments, Action
 from trace_parser import parse_trace
 from scheduler import simulate_scheduler
 from output_builder import write_output
 
+name = "Schedule Simulator"
+version = "0.0.1"
+
 if __name__ == "__main__":
     
-    argument_namespace = parse_arguments(sys.argv)
+    argument_action = parse_arguments(sys.argv)
     # Decide what to do based on parsed arguments...
-    pass
+
+    match argument_action[0]:
+        case Action.HELP:
+            pass
+        case Action.VERSION:
+            print(f"{name} {version}")
+        case Action.USE_TRACE:
+            pass
+        case Action.USE_CONFIG:
+            pass
