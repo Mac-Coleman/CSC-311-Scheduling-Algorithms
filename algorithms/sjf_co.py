d9b7471596370062194c78c5026c174b4d7a0c21
TdgCC <85917263+TdgCC@users.noreply.github.com>
2023-09-10 18:27:45 -0500

    Update sjf_co.py

--- algorithms/sjf_co.py
+++ algorithms/sjf_co.py
@@ -4,5 +4,25 @@ Assigned maintainer: Torii
 
 from process import Process, ProcessExecutionRecord
 
-def simulate_sjf_co(arriving_processes: list[Process], args: list[str]) -> tuple[list[ProcessExecutionRecord], dict[int, int]]:
-    pass
+def simulate_sjf_co(arriving_processes: [Process]) -> ([ProcessExecutionRecord], {int : int}):
+    processes_sorted=[Process]
+    per_list=[ProcessExecutionRecord]
+    wait_times={int:int}
+    i=0
+    for p in arriving_processes:
+        if(p.burst_time<i):
+            processes_sorted.insert(i,p)
+        else:
+            processes_sorted.insert(i+1,p)
+    clock=0
+    while(len(processes_sorted)>0):
+        found=False
+        for p in processes_sorted:
+            if(p.arrival_time<=clock):
+                if(found==False):
+                    per_list.append(ProcessExecutionRecord(p.pid,clock,p.burst_time))
+                    clock+=p.burst_time
+                    wait_times.update({p.pid:(clock-p.arrival_time)})
+                    processes_sorted.remove(p)
+                    
+    return {per_list,wait_times}
