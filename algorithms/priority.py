6ac63adf61ebc0cf827a3e0cc9d1bf1c0a7f7933
mccuen1 <mccuen.brodie@gmail.com>
2023-09-12 22:00:29 -0500

    made priority scheduler work

--- algorithms/priority.py
+++ algorithms/priority.py
@@ -1,7 +1,7 @@
 
 from process import Process, ProcessExecutionRecord
 from trace_parser import parse_trace
-
+from math import inf
 
 def simulate_priority(arriving_processes: list[Process], args: list[str]) -> tuple[list[ProcessExecutionRecord], dict[int, int]]:
     process_queue = []
@@ -9,47 +9,47 @@ def simulate_priority(arriving_processes: list[Process], args: list[str]) -> tup
     index = 0
     total_wait_times = {}
     wait_times = {}
-    burst = 0
+    cpu_burst = 0
     schedule = []
     
     # Main Loop
-    while process_queue != [] or time < 100:
-        print("HERE")
-        print("time", time)
+    #   process queue not empty?   process still running?
+    while len(process_queue) > 0 or cpu_burst >= 0 or index < len(arriving_processes):
         # admit a new process to queue if it matches the time
         if index < len(arriving_processes) and arriving_processes[index].arrival_time == time:
-            print("subset: ", subset(arriving_processes[index:]))
-            for new_process in subset(arriving_processes[index:]):
+            same_arrival_time = subset(arriving_processes[index:])
+            print(same_arrival_time)
+            for new_process in same_arrival_time:
                 wait_times[new_process.pid] = 0
                 process_queue.append(new_process)
+                index += 1
 
-        if burst > 0:
-            burst -= 1
+        # time remaining of current process
+        cpu_burst -= 1
 
         # first process -> set initial variables
         if index == 0:
             start_time = time
             current_process = choose_new_process(process_queue)
-            burst = current_process.burst_time
-            index += 1
+            cpu_burst = current_process.burst_time
             
 
         
         # when a process finishes
-        if burst == 0:
+        if cpu_burst == 0:
             # write down the record of the process
-            schedule.append(current_process.to_record(start_time, current_process.burst_time))
-            if process_queue != []:
-                # choose new process
-                current_process = choose_new_process(process_queue)
-                process_queue.remove(current_process)
-                # move wait times to total wait times
-                total_wait_times[current_process.pid] = wait_times[current_process.pid]
-                wait_times.pop(current_process.pid)
-                # reset variables
-                burst = current_process.burst_time
-                start_time = time
-                index += 1
+            schedule.append(current_process.to_record(start_time, current_process.execute(current_process.burst_time)))
+        if process_queue != [] and cpu_burst <= 0:
+            # choose new process
+            current_process = choose_new_process(process_queue)
+            process_queue.remove(current_process)
+            # move wait times to total wait times
+            total_wait_times[current_process.pid] = wait_times[current_process.pid]
+            wait_times.pop(current_process.pid)
+            # reset variables
+            cpu_burst = current_process.burst_time
+            start_time = time
+            
             
 
         # increment wait times by 1 each cycle
@@ -63,24 +63,24 @@ def simulate_priority(arriving_processes: list[Process], args: list[str]) -> tup
 
 ### Creates a subset
 def subset(arriving_processes: list[Process]):
-    time = arriving_processes[0]
-    index = 1
-    process_list = [arriving_processes[0]]
-    try:
-        while arriving_processes[index].arrival_time == time:
-            process_list.append(arriving_processes[index])
-            index += 1
-        return process_list
-    except:
-        return process_list
+    time = arriving_processes[0].arrival_time
+    process_list = []
+    for process in arriving_processes:
+        if process.arrival_time == time:
+            process_list.append(process)
+        else:
+            return process_list
+    # assurance reasons
+    return process_list
+
 
 
 ### Find the process in the queue with the highest priority
 ### Process queue automatically sorted by arrival time
 def choose_new_process(process_queue):
-    highest_prio = 0
+    highest_prio = inf
     for process in process_queue:
-        if process.priority > highest_prio:
+        if process.priority < highest_prio:
             highest_prio = process.priority
             highest = process
 
