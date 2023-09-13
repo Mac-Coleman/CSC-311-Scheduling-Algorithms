665aa358c6b1760d410268d69a27471f5574a44f
TdgCC <85917263+TdgCC@users.noreply.github.com>
2023-09-12 21:06:42 -0500

    SJF-co completed, SJF-pr started

--- algorithms/sjf_co.py
+++ algorithms/sjf_co.py
@@ -5,34 +5,33 @@ Assigned maintainer: Torii
 from process import Process, ProcessExecutionRecord
 
 def simulate_sjf_co(arriving_processes: list[Process], args: list[str]) -> tuple[list[ProcessExecutionRecord], dict[int,int]]:
-    processes_sorted: list[Process] = []
+    sorted_processes: list[Process] = []
     per_list: list[ProcessExecutionRecord] = []
     wait_times: dict[int,int] = {}
     for p in arriving_processes:
         i=0
-        while(i<len(processes_sorted)):
-            if(p.burst_time<processes_sorted[i].burst_time):
-                processes_sorted.insert(i,p)
-                i=len(processes_sorted)
+        while(i<len(sorted_processes)):
+            if(p.burst_time<sorted_processes[i].burst_time):
+                sorted_processes.insert(i,p)
+                i=len(sorted_processes)
             i+=1
-        if(i==len(processes_sorted)):
-            processes_sorted.insert(i,p)
+        if(i==len(sorted_processes)):
+            sorted_processes.insert(i,p)
 
     clock=0    
-    print(processes_sorted)
 
 
-    while(len(processes_sorted)>0):
+    while(len(sorted_processes)>0):
         found=False
-        for p in processes_sorted:
+        for p in sorted_processes:
             if(p.arrival_time<=clock):
                 if(found==False):
                     run_time=p.execute(p.burst_time)
                     per_list.append(p.to_record(clock, run_time))
+                    wait_times.update({p.pid:(clock-p.arrival_time)}) 
                     clock+=run_time
-                    wait_times.update({p.pid:(clock-p.arrival_time)})
-                    processes_sorted.remove(p)
+                    sorted_processes.remove(p)
                     found=True
-            if(found==False):
-               clock+=1
+        if(found==False):
+            clock+=1        
     return (per_list,wait_times)
--- algorithms/sjf_pr.py
+++ algorithms/sjf_pr.py
@@ -5,4 +5,49 @@ Assigned maintainer: Torii
 from process import Process, ProcessExecutionRecord
 
 def simulate_sjf_pr(arriving_processes: list[Process], args: list[str]) -> tuple[list[ProcessExecutionRecord], dict[int, int]]:
-    pass
+    clock=0
+    current_process_start=0
+    current_process_time=0
+    current_process=None
+    available_processes: list[Process] = []
+    wait_times: dict[int,int] = {}
+    per_list: list[ProcessExecutionRecord] = []
+    while(len(arriving_processes)>0 or len(available_processes)>0):
+        dead_list: list[Process]=[]
+        for p in arriving_processes:# adding any process that has arrived in this clock cycle to available_processes
+            if(wait_times.get(p.pid)==None):
+                wait_times[p.pid]=0           
+            if(p.burst_time<=clock):
+                available_processes.append(p)
+                dead_list.append(p)
+        for p in dead_list:
+            arriving_processes.remove(p)
+        best_process=None
+        best_time=float('inf')
+        for p in available_processes:# check if any available processes have a shorter burst time then the current one
+            if(p.burst_time<best_time):
+                best_process=p
+                best_time=p.burst_time  
+        # swap if a shorter job is found or if current process has ended
+        if(current_process!=None):
+           if(best_time<current_process.burst_time or current_process.burst_time==0):
+                #per_list.append(current_process.to_record(current_process_start,current_process_time))
+                if(current_process.burst_time>0):# if current_process has time left, it goes back in line. Otherwise it disappears after being recorded to per_list
+                    available_processes.append(current_process)
+                    print("yes")
+                current_process=best_process
+                current_process_start=clock
+                current_process_time=0
+                available_processes.remove(best_process)
+        else:
+            current_process=best_process
+            current_process_start=clock
+            current_process_time=0
+        for p in available_processes:# updates the waits for all processes in the available list
+            wait_times[p.pid]=wait_times.get(p.pid)+1
+        if(current_process!=None):#only needs to execute for a second because the loop recalculates every clock cycle
+            current_process.execute(1)   
+            current_process_time+=1
+            per_list.append(current_process.to_record(current_process_start,current_process_time))
+        clock+=1
+    return(per_list,wait_times)
